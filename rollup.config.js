a442adfca4c5caaa369cad0e8f78d5f67101503e
Alice <alice@ritsu.dev>
2024-03-26 20:34:42 +1100

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+	// This is adapted from the sample config: https://www.rollupjs.org/configuration-options/#input
+	input: 'src/index.ts',
+	output: {
+		dir: 'lib',
+		format: 'es',
+		preserveModules: true,
+		preserveModulesRoot: 'src'
+	},
+	external: [
+    '@react-native-async-storage/async-storage',
+    'axios',
+    'react',
+    'zustand',
+    'zustand/middleware'
+	],
+	plugins: [ typescript() ]
+};
