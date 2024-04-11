d3c39a847d7b14b66279f12d0439ae8edeb30d9f
Alice <alice@sleepyand.gay>
2024-04-11 23:23:37 +1000

    fix types

--- src/api/system.ts
+++ src/api/system.ts
@@ -4,7 +4,7 @@ import Client from '../types/Client'
 import InfoPublic from '../types/system/InfoPublic'
 
 export const info = (client: Client) => {
-  return new Promise<InfoPublic | AxiosError>((resolve) => {
+  return new Promise<InfoPublic>((resolve) => {
     client.client.get('/System/Info')
       .then((res) => resolve(res.data))
       .catch((error: AxiosError) => resolve(error))
@@ -12,7 +12,7 @@ export const info = (client: Client) => {
 }
 
 export const infoPublic = (server: string) => {
-  return new Promise<InfoPublic | AxiosError>((resolve) => {
+  return new Promise<InfoPublic>((resolve) => {
     axios
       .get<InfoPublic>('/System/Info/Public', { baseURL: server })
       .then((res) => resolve(res.data))
