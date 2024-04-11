19948c50a0e7bf78029dd7ebc3be76568685bef6
Alice <alice@sleepyand.gay>
2024-04-11 23:26:10 +1000

    actually fix types

--- src/api/system.ts
+++ src/api/system.ts
@@ -4,18 +4,18 @@ import Client from '../types/Client'
 import InfoPublic from '../types/system/InfoPublic'
 
 export const info = (client: Client) => {
-  return new Promise<InfoPublic>((resolve) => {
+  return new Promise<InfoPublic>((resolve, reject) => {
     client.client.get('/System/Info')
       .then((res) => resolve(res.data))
-      .catch((error: AxiosError) => resolve(error))
+      .catch((error: AxiosError) => reject(error))
   })
 }
 
 export const infoPublic = (server: string) => {
-  return new Promise<InfoPublic>((resolve) => {
+  return new Promise<InfoPublic>((resolve, reject) => {
     axios
       .get<InfoPublic>('/System/Info/Public', { baseURL: server })
       .then((res) => resolve(res.data))
-      .catch((error: AxiosError) => resolve(error))
+      .catch((error: AxiosError) => reject(error))
   })
 }
