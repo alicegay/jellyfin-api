04edade2d43d46f40d23eeaea735497a35ca2fad
Alice <alice@ritsu.dev>
2024-04-15 01:24:38 +1000

    fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import * as artists from './api/artists'
+import * as items from './api/items'
 import * as playlists from './api/playlists'
 import * as shows from './api/shows'
 import * as system from './api/system'
 import * as users from './api/users'
 import type Client from './types/Client'
 
-export {
-  artists,
-  playlists,
-  shows,
-  system,
-  users,
-  Client
-}
+export { artists, items, playlists, shows, system, users, Client }
