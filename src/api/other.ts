2863833f5d4f755bff871f942e56630a7d6a5f87
Alice <alice@ritsu.dev>
2024-05-04 16:05:22 +1000

    fix api

--- src/api/other.ts
+++ src/api/other.ts
@@ -5,7 +5,7 @@ import IntroTimestamps from '../types/other/IntroTimestamps'
 export const introTimestamps = (client: Client, itemID: string) => {
   return new Promise<IntroTimestamps>((resolve, reject) => {
     client.client
-      .get<IntroTimestamps>('/Episodes/' + itemID + '/IntroTimestamps/v1')
+      .get<IntroTimestamps>('/Episode/' + itemID + '/IntroTimestamps/v1')
       .then(
         (res) => resolve(res.data),
         (error: AxiosError) => reject(error),
